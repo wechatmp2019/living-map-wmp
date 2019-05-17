2bc3da62eec107411e93a1fd25345cdb8c16f1bd
chenjiaxi <chenjiaxi_byr@163.com>
2019-05-18 00:15:53 +0800

    feat(login): archieve login function using vuex

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
-  data () {
-    return {
-      global: {}
-    };
-  },
-  created () {
+    data () {
+        return {
+            global: {}
+        };
+    },
+    computed: {
+        ...mapState([
+            'openId'
+        ])
+    },
+    methods: {
+        ...mapActions([
+            'login'
+        ]),
+        _login () {
+            wx.login({
+                success: (res) => {
+                    if (res.code) {
+                        console.log('wx.login success,code:', res.code);
+                        this.login(res.code);
+                    } else {
+                        this.$tips.toast('微信登录失败');
+                    }
+                }
+            });
+        }
+    },
+    created () {
     // 调用API从本地缓存中获取数据
     /*
      * 平台 api 差异的处理方式:  api 方法统一挂载到 mpvue 名称空间, 平台判断通过 mpvuePlatform 特征字符串
@@ -15,22 +39,21 @@ export default {
      * 支付宝(蚂蚁)：mpvue === my, mpvuePlatform === 'my'
      */
 
-    let logs;
-    if (mpvuePlatform === 'my') {
-      logs = mpvue.getStorageSync({key: 'logs'}).data || [];
-      logs.unshift(Date.now());
-      mpvue.setStorageSync({
-        key: 'logs',
-        data: logs
-      });
-    } else {
-      logs = mpvue.getStorageSync('logs') || [];
-      logs.unshift(Date.now());
-      mpvue.setStorageSync('logs', logs);
-    }
-  },
-
-  mounted () {
+        let logs;
+        if (mpvuePlatform === 'my') {
+            logs = mpvue.getStorageSync({key: 'logs'}).data || [];
+            logs.unshift(Date.now());
+            mpvue.setStorageSync({
+                key: 'logs',
+                data: logs
+            });
+        } else {
+            logs = mpvue.getStorageSync('logs') || [];
+            logs.unshift(Date.now());
+            mpvue.setStorageSync('logs', logs);
+        }
+    },
+    mounted () {
     // 自定义导航栏
     // wx.getSystemInfo({
     //   success: e => {
@@ -40,11 +63,12 @@ export default {
     //     this.global.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
     //   }
     // });
-  },
+    },
 
-  log () {
-    console.log(`log at:${Date.now()}`);
-  }
+    onLaunch () {
+        this._login();
+    }
+
 };
 </script>
 
@@ -69,3 +93,4 @@ export default {
   -o-transition: width 2s;
 }
 </style>
+
