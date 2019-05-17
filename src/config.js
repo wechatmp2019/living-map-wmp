de0578a6ab59468c0c18c36cf81dcca217ae4425
chenjiaxi <chenjiaxi_byr@163.com>
2019-05-18 00:12:23 +0800

    feat(config): global config

---
+++ src/config.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: chenjiaxi
+ * @Date: 2019-05-15 19:50:02
+ * @Last Modified by: chenjiaxi
+ * @Last Modified time: 2019-05-15 22:03:51
+ */
+
+const host = ''; // baseURL
+const appid = '';
+const appkey = '';
+
+const config = {
+    host,
+    appid,
+    appkey
+};
+
+export default config;
