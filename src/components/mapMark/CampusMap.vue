026b708614aad431e6af8652e7bdd7b2e74b71a8
chenjiaxi <chenjiaxi_byr@163.com>
2019-05-21 21:31:30 +0800

    feat(map): add map and mapMark comp

---
+++ src/components/mapMark/CampusMap.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="campus-map" >
+        <image src="/static/images/mockMap.jpg" mode="widthFix" class="campus-map__img"/>
+        <map-mark :position="[6, 6]"/>
+        <map-mark :position="[20, 20]"/>
+        <map-mark :position="[50, 50]"/>
+    </div>
+</template>
+
+<script>
+import MapMark from './MapMark';
+
+export default {
+    name: 'campus-map',
+    components: {
+        MapMark
+    },
+    props: {
+        campus: {
+            type: String,
+            default: 'bupt'
+        },
+        mapMarks: {
+            type: Array,
+            default: []
+        }
+    },
+    computed: {
+    },
+    methods: {
+    }
+};
+</script>
+
+<style scoped>
+.campus-map {
+    position: relative;
+}
+.campus-map__img {
+    width: 100vw;
+}
+</style>
---
+++ src/components/mapMark/MapMark.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="map-mark" :style="style">
+        <i-icon type="coordinates_fill" :size="size" :custom="icon" :color="color"/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'map-mark',
+    components: {
+    },
+    props: {
+        icon: {
+            type: String,
+            default: ''
+        },
+        place: {
+            type: String,
+            default: ''
+        },
+        position: {
+            type: Array,
+            default: [10, 10]
+        },
+        size: {
+            type: String,
+            default: '66'
+        },
+        color: {
+            type: String,
+            default: '#B2D0EA'
+        }
+    },
+    computed: {
+        style () {
+            return this.$styles({
+                left: this.position[0] + 'vw',
+                top: this.position[1] + 'vh'
+            });
+        }
+    },
+    methods: {
+    }
+};
+</script>
+
+<style scoped>
+.map-mark {
+    position: absolute;
+}
+
+</style>
