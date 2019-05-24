ce46df6d920b2459ee0af673540f0e843096bed3
ChuankaiCMU <chuankaz@andrew.cmu.edu>
2019-05-24 00:57:24 -0400

    Added 4 pages

---
+++ src/pages/card/contactuspage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+        <ContactUs></ContactUs>
+    </div>
+  </div>
+</template>
+
+<script>
+    import ContactUs from '@/components/cardetails/ContactUs';
+    export default({
+        components: {
+            ContactUs
+        }
+});
+</script>
---
+++ src/pages/card/mycardpage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+        <MyCard></MyCard>
+    </div>
+  </div>
+</template>
+
+<script>
+    import MyCard from '@/components/cardetails/MyCard';
+    export default {
+        data () {
+            return {
+                name: 'name1',
+                list: {
+                    cardtype: [{
+                        id: 1,
+                        name: 'aaa',
+                        content: 'aaaaaaaa'
+                    }, {
+                        id: 2,
+                        name: 'bbb',
+                        content: 'bbbbbbbb'
+                    }, {
+                        id: 3,
+                        name: 'ccc',
+                        content: 'cccccccc'
+                    }
+                    ]
+                }
+            };
+        },
+        components: {
+            MyCard
+        },
+        methods: {
+            handleClick () {
+            }
+        }
+    };
+</script>
